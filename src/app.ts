618d75cffe9dd6bf28d36a51204b96651872f32f
Rahad-Ullah <myemail.abc20@gmail.com>
2024-11-12 00:19:03 +0600

    Not found error handled

--- src/app.ts
+++ src/app.ts
@@ -11,13 +11,24 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.get("/", (req: Request, res: Response) => {
+  res.send({
+    message: "PH Health Care Server is running",
+  });
+});
+
 app.use("/api/v1", router);
 
 app.use(globalErrorHandler);
 
-app.get("/", (req: Request, res: Response) => {
-  res.send({
-    message: "PH Health Care Server is running",
+app.use((req: Request, res: Response, next: NextFunction) => {
+  res.status(400).json({
+    success: false,
+    message: "API not found",
+    error: {
+      path: req.originalUrl,
+      message: "Your requested API is not available",
+    },
   });
 });
 
