8a0fe32cb5a9106470ffa53f007ffb1962852a44
Rahad-Ullah <myemail.abc20@gmail.com>
2024-11-11 16:53:21 +0600

    formated response

--- src/app/modules/User/user.controller.ts
+++ src/app/modules/User/user.controller.ts
@@ -2,8 +2,20 @@ import { Request, Response } from "express";
 import { userService } from "./user.service";
 
 const createAdmin = async (req: Request, res: Response) => {
-  const result = await userService.createAdminIntoDB(req.body);
-  res.send(result);
+  try {
+    const result = await userService.createAdminIntoDB(req.body);
+    res.status(200).json({
+      success: true,
+      message: "Admin created successfully",
+      data: result,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error?.name || "Error creating admin",
+      error: error,
+    });
+  }
 };
 
 export const userController = {
