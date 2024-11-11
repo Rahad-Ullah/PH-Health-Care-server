45e3941714d7fc340050af87d82fb9ec3a9ed77c
Rahad-Ullah <myemail.abc20@gmail.com>
2024-11-11 23:57:16 +0600

    Configuring routes file

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express, { Application, Request, Response } from "express";
 import cors from "cors";
 import { userRoutes } from "./app/modules/User/user.route";
 import { adminRoutes } from "./app/modules/Admin/admin.route";
+import router from "./app/routes";
 
 const app: Application = express();
 
@@ -11,8 +12,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use("/api/v1/user", userRoutes);
-app.use("/api/v1/admin", adminRoutes);
+app.use("/api/v1", router);
 
 app.get("/", (req: Request, res: Response) => {
   res.send({
