241e32bc1aa242e9ca2d28a5138f9731974243d4
Rahad-Ullah <myemail.abc20@gmail.com>
2024-11-11 20:32:48 +0600

    Added more fields in searching

--- src/app/modules/Admin/admin.service.ts
+++ src/app/modules/Admin/admin.service.ts
@@ -1,31 +1,41 @@
-import { PrismaClient } from "@prisma/client";
+import { Prisma, PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
 const getAllAdminsFromDB = async (params: any) => {
-  console.log(params);
+  const { searchTerm, ...filterData } = params;
+  const conditions: Prisma.AdminWhereInput[] = [];
+  const adminSearchableFields = ["name", "email"];
+
+  if (params.searchTerm) {
+    conditions.push({
+      OR: adminSearchableFields.map((value) => ({
+        [value]: {
+          contains: params.searchTerm,
+          mode: "insensitive",
+        },
+      })),
+    });
+  }
+
+  if (Object.keys(filterData).length > 0) {
+    conditions.push({
+      AND: Object.keys(filterData).map((key) => ({
+        [key]: {
+          equals: filterData[key],
+        },
+      })),
+    });
+  }
+
+  const whereConditions: Prisma.AdminWhereInput = { AND: conditions };
 
   const result = await prisma.admin.findMany({
-    where: {
-      OR: [
-        {
-          name: {
-            contains: params.searchTerm,
-            mode: "insensitive",
-          },
-        },
-        {
-          email: {
-            contains: params.searchTerm,
-            mode: "insensitive",
-          },
-        },
-      ],
-    },
+    where: whereConditions,
   });
   return result;
 };
 
 export const adminServices = {
-    getAllAdminsFromDB
-}
+  getAllAdminsFromDB,
+};
