8711c631c3497e7ec514dc2be377d40de7d41aa4
Rahad-Ullah <myemail.abc20@gmail.com>
2024-11-11 16:43:46 +0600

    Hashed password

--- src/app/modules/User/user.service.ts
+++ src/app/modules/User/user.service.ts
@@ -1,11 +1,15 @@
 import { PrismaClient, UserRole } from "@prisma/client";
+import * as bcrypt from "bcrypt";
 
 const prisma = new PrismaClient();
 
 const createAdminIntoDB = async (data: any) => {
+  const hashedPassword: string = await bcrypt.hash(data.password, 12);
+  console.log({ hashedPassword });
+
   const userData = {
     email: data.admin.email,
-    password: data.password,
+    password: hashedPassword,
     role: UserRole.ADMIN,
   };
 
