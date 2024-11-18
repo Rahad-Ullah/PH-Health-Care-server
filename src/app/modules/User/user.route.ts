af7155d17f76ec8410824f409eef91300eb959d0
Rahad-Ullah <myemail.abc20@gmail.com>
2024-11-18 16:34:10 +0600

    File uploading using multer

--- src/app/modules/User/user.route.ts
+++ src/app/modules/User/user.route.ts
@@ -2,12 +2,16 @@ import express from "express";
 import { userController } from "./user.controller";
 import auth from "../../middlewares/auth";
 import { UserRole } from "@prisma/client";
+import { fileUploader } from "../../../utils/fileUploader";
 
 const router = express.Router();
+
+
 
 router.post(
   "/",
   auth(UserRole.SUPER_ADMIN, UserRole.ADMIN),
+  fileUploader.upload.single('file'),
   userController.createAdmin
 );
 
