1f9633ffc9c1f9fb03d1abe68cedcbecade9d84a
Rahad-Ullah <myemail.abc20@gmail.com>
2024-11-21 08:11:42 +0600

    Fixed type error and duplicate entry

--- src/app/modules/Specialities/specialities.route.ts
+++ src/app/modules/Specialities/specialities.route.ts
@@ -1,6 +1,7 @@
 import express, { NextFunction, Request, Response } from "express";
 import { SpecialitiesControllers } from "./specialities.controller";
 import { fileUploader } from "../../../utils/fileUploader";
+import { SpecialitiesValidations } from "./specialities.validation";
 
 const router = express.Router();
 
@@ -8,7 +9,9 @@ router.post(
   "/",
   fileUploader.upload.single("file"),
   (req: Request, res: Response, next: NextFunction) => {
-    req.body = JSON.parse(req.body.data);
+    req.body = SpecialitiesValidations.createSpeciality.parse(
+      JSON.parse(req.body.data)
+    );
     return SpecialitiesControllers.createSpeciality(req, res, next);
   }
 );
--- src/app/modules/Specialities/specialities.service.ts
+++ src/app/modules/Specialities/specialities.service.ts
@@ -1,11 +1,22 @@
 import { Request } from "express";
 import { fileUploader } from "../../../utils/fileUploader";
 import prisma from "../../../shared/prisma";
+import { IUploadedFile } from "../../interfaces/file";
+import ApiError from "../../errors/ApiError";
+import { StatusCodes } from "http-status-codes";
 
 // create a new speciality
 const createSpecialityIntoDB = async (req: Request) => {
+  // check if the speciality already exists
+  const speciality = await prisma.specialities.findFirst({
+    where: { title: req.body.title },
+  });
+  if (speciality) {
+    throw new ApiError(StatusCodes.CONFLICT, "The speciality already exists");
+  }
+
   // upload icon file to cloudinary
-  const file = req.file;
+  const file = req.file as IUploadedFile;
   if (file) {
     const uploadedToCloudinary = await fileUploader.uploadToCloudinary(file);
     req.body.icon = uploadedToCloudinary?.secure_url;
