80550a0635b3cb426dc3e249b09096a72b8281ef
Rahad-Ullah <myemail.abc20@gmail.com>
2024-11-23 12:40:40 +0600

    Creating doctor schedule with validation

--- src/app/modules/DoctorSchedule/doctorSchedule.service.ts
+++ src/app/modules/DoctorSchedule/doctorSchedule.service.ts
@@ -1,13 +1,43 @@
+import { StatusCodes } from "http-status-codes";
 import prisma from "../../../shared/prisma";
+import ApiError from "../../errors/ApiError";
 
 const createDoctorScheduleIntoDB = async (
   user: any,
   payload: { scheduleIds: string[] }
 ) => {
-  const doctor = await prisma.doctor.findUnique({
+  // check if doctor exists
+  const doctor = await prisma.doctor.findUniqueOrThrow({
     where: { email: user.email },
   });
-  console.log(payload.scheduleIds);
+
+  // check if schedule ids is valid
+  for (const scheduleId of payload.scheduleIds) {
+    const schedule = await prisma.schedule.findUnique({
+      where: {
+        id: scheduleId,
+      },
+    });
+    if (!schedule) {
+      throw new ApiError(
+        StatusCodes.NOT_FOUND,
+        `Schedule ID '${scheduleId}' does not exist`
+      );
+    }
+  }
+
+  // make doctor schedule list
+  const doctorScheduleData = payload.scheduleIds.map((scheduleId) => ({
+    doctorId: doctor.id,
+    scheduleId,
+  }));
+
+  // create into database
+  const result = await prisma.doctorSchedules.createMany({
+    data: doctorScheduleData,
+  });
+
+  return result;
 };
 
 export const DoctorScheduleServices = {
