f81bf60095cc871255bd6bedfbbb8ba147dce24f
Rahad-Ullah <myemail.abc20@gmail.com>
2024-11-23 11:02:38 +0600

    Fixed duplicate schedule entry and type safety

--- src/app/modules/Schedule/schedule.service.ts
+++ src/app/modules/Schedule/schedule.service.ts
@@ -1,7 +1,11 @@
 import { addHours, addMinutes, format } from "date-fns";
 import prisma from "../../../shared/prisma";
+import { Schedule } from "@prisma/client";
+import { ISchedule } from "./schedule.interface";
 
-const createScheduleIntoDB = async (payload: any) => {
+const createScheduleIntoDB = async (
+  payload: ISchedule
+): Promise<Schedule[]> => {
   const { startDate, endDate, startTime, endTime } = payload;
 
   const slotInterval = 30;
@@ -13,16 +17,25 @@ const createScheduleIntoDB = async (payload: any) => {
 
   // loop for date range
   while (currentDate <= lastDate) {
-    // add hour with date
+    // add hour-minute with date
     const startDateTime = new Date(
-      addHours(
-        format(currentDate, "yyyy-MM-dd"),
-        Number(startTime.split(":")[0])
+      addMinutes(
+        addHours(
+          format(currentDate, "yyyy-MM-dd"),
+          Number(startTime.split(":")[0])
+        ),
+        Number(startTime.split(":")[1])
       )
     );
-    // add hour with date
+    // add hour-minute with date
     const endDateTime = new Date(
-      addHours(format(currentDate, "yyyy-MM-dd"), Number(endTime.split(":")[0]))
+      addMinutes(
+        addHours(
+          format(currentDate, "yyyy-MM-dd"),
+          Number(endTime.split(":")[0])
+        ),
+        Number(endTime.split(":")[1])
+      )
     );
 
     // loop for time range
@@ -32,13 +45,23 @@ const createScheduleIntoDB = async (payload: any) => {
         endDateTime: addMinutes(startDateTime, slotInterval),
       };
 
-      // create schedule in DB
-      const result = await prisma.schedule.create({
-        data: scheduleData,
+      // check if schedule already exists in the database
+      const isScheduleExists = await prisma.schedule.findFirst({
+        where: {
+          startDateTime: scheduleData.startDateTime,
+          endDateTime: scheduleData.endDateTime,
+        },
       });
+      // create schedule if it doesn't exist
+      if (!isScheduleExists) {
+        // create schedule in DB
+        const result = await prisma.schedule.create({
+          data: scheduleData,
+        });
 
-      // push to schedules array
-      schedules.push(result);
+        // push to schedules array
+        schedules.push(result);
+      }
 
       // increase start time by interval
       startDateTime.setMinutes(startDateTime.getMinutes() + slotInterval);
