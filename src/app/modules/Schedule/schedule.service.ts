fa27d00abe13f7933afc354cceb592e55403709b
Rahad-Ullah <myemail.abc20@gmail.com>
2024-11-23 10:41:40 +0600

    Schedule generated with multiple dates

--- src/app/modules/Schedule/schedule.service.ts
+++ src/app/modules/Schedule/schedule.service.ts
@@ -1,26 +1,55 @@
-import { addHours, format } from "date-fns";
+import { addHours, addMinutes, format } from "date-fns";
+import prisma from "../../../shared/prisma";
 
 const createScheduleIntoDB = async (payload: any) => {
   const { startDate, endDate, startTime, endTime } = payload;
 
-  const currentDate = new Date(startDate);
-  const lastDate = new Date(endDate);
+  const slotInterval = 30;
 
+  const schedules = []; //  all schedules is stored here and we return a list of schedules
+
+  const currentDate = new Date(startDate); // start date
+  const lastDate = new Date(endDate); // end date
+
+  // loop for date range
   while (currentDate <= lastDate) {
+    // add hour with date
     const startDateTime = new Date(
       addHours(
         format(currentDate, "yyyy-MM-dd"),
         Number(startTime.split(":")[0])
       )
     );
-
+    // add hour with date
     const endDateTime = new Date(
-      addHours(format(lastDate, "yyyy-MM-dd"), Number(endTime.split(":")[0]))
+      addHours(format(currentDate, "yyyy-MM-dd"), Number(endTime.split(":")[0]))
     );
 
-    console.log(startDateTime);
-    console.log(endDateTime);
+    // loop for time range
+    while (startDateTime < endDateTime) {
+      const scheduleData = {
+        startDateTime: startDateTime,
+        endDateTime: addMinutes(startDateTime, slotInterval),
+      };
+
+      // create schedule in DB
+      const result = await prisma.schedule.create({
+        data: scheduleData,
+      });
+
+      // push to schedules array
+      schedules.push(result);
+
+      // increase start time by interval
+      startDateTime.setMinutes(startDateTime.getMinutes() + slotInterval);
+    }
+
+    // increase current date by 1
+    currentDate.setDate(currentDate.getDate() + 1);
   }
+
+  // return schedule list
+  return schedules;
 };
 
 export const scheduleServices = {
